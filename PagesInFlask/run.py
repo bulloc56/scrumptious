72c8fdd5820e25a8458e5b256b54f96ec30fc6c9
bulloc56 <kevinbullock3@gmail.com>
2020-05-12 22:46:05 -0400

    Update run.py

--- PagesInFlask/run.py
+++ PagesInFlask/run.py
@@ -1,5 +1,6 @@
 from app import app, socketio
-
+import os
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get(“PORT”, 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
 #must add host='0.0.0.0', port=5000, inside to run on AWS
